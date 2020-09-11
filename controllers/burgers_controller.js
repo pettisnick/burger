5ffc89df2bc64600bacab1d294bcc5763450d30f
Nick Pettis <pettisnick@gmail.com>
2020-09-10 22:42:30 -0400

    editing files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const burgers = require("../db/models/burger");
+const router = express.Router();
+
+//Export Router
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,8 @@
+//Import ORM
+const orm = require("../config/orm");
+
+//Create the code that will call the ORM functions using burger specific input for the ORM
+
+
+//Export burgers.js
+module.exports = burgers;
